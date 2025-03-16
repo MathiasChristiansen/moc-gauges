efee9b23b894e86120e75a0068919a878c174708
Mathias Ólavsson Christiansen <mathias.olavsson@gmail.com>
2025-03-16 14:46:34 +0000

    Proper exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ export const helloGauges: () => string = () => {
   console.log("Hello Gauges!");
   return "Hello Gauges!";
 };
+
+export { SpeedometerGauge } from "./Gauges/Speedometer";
+export { WindGauge } from "./Gauges/Wind";
+export { CompassGauge } from "./Gauges/Compass";
